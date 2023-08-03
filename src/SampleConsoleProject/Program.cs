8a2e6f845d0518b77459bb944a1ef663b4a3385a
pluto <38720155+pluto-dev@users.noreply.github.com>
2023-08-03 13:17:49 +0200

    Add sample project

---
+++ src/SampleConsoleProject/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using PodcastIndex.Net;
+
+namespace SampleConsoleProject 
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = Host.CreateDefaultBuilder()
+                .ConfigureServices(
+                    (context, services) =>
+                    {
+                        services.AddPodcastIndexNet(context.Configuration);
+                    }
+                )
+                .Build();
+
+            ActivatorUtilities.CreateInstance<Test>(host.Services);
+        }
+    }
+
+    class Test
+    {
+        public Test(IPodcastIndex podcastIndex)
+        {
+            var search = podcastIndex.Search().GetByTermAsync("Batman").Result;
+        }
+    }
+}
